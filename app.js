a01f6f0928821fc0b674624f28ebc47793e210a1
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-11-27 17:11:27 +0000

    Cash the dom and sore them in variable for future use

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-let list = document.getElementById("list");
-console.log(list);
+const userScore = 0;
+const computerScore = 0;
+const userScoreSpan = document.getElementById("user-score");
+const computerScoreSpan = document.getElementById("computer-score");
+const scoreBoardDiv = document.querySelector(".score-board");
+const resultDiv = document.querySelector(".result");
+const rockDiv = document.getElementById("r");
+const paperDiv = document.getElementById("p");
+const scissorsDiv = document.getElementById("s");
